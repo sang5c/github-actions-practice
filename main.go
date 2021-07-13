d5c7252a206fb28212abce6eeb5e93a8c679ac6d
sang5c <skah321@gmail.com>
2021-07-13 11:01:15 +0900

    go http hello world

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hello World!")
+	})
+	http.ListenAndServe(":8080", nil)
+}
